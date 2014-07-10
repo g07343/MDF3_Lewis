d6a437221f03ba1da5b10bc7c84fb91c833c2e48
Matthew Lewis <Loneeagle14@aol.com>
2014-07-10 00:47:00 -0500

    Enlarged drawer and created icons
    Made the “drawer” larger within the Decorate activity so that the user
    has enough screen space to scroll using the scrollview.  Added 6 icons
    (for now) to serve as items the user can overlay onto their image.

--- Week 1/PhotoSet/src/com/matthewlewis/photoset/DecorateActivity.java
+++ Week 1/PhotoSet/src/com/matthewlewis/photoset/DecorateActivity.java
@@ -13,14 +13,25 @@
  */
 package com.matthewlewis.photoset;
 
+import java.util.ArrayList;
+
 import com.matthewlewis.photomail.R;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class DecorateActivity extends Activity{
 	
+	LinearLayout iconDrawer;
+	Button finishBtn;
+	Integer currentSelected;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +40,51 @@ public class DecorateActivity extends Activity{
 		
 		//set our view to the xml layout
 		setContentView(R.layout.activity_decorate);
+		
+		//grab references to our views that we may need
+		finishBtn = (Button) findViewById(R.id.decorate_setBtn);
+		iconDrawer = (LinearLayout) findViewById(R.id.decorate_drawerHolder);
+		
+		iconDrawer.setVisibility(View.GONE);
+		
+		//this onClickListener is needed so that the buttons underneath the view don't receive tap events, since 
+		//Android delegates tap events to them otherwise.
+		iconDrawer.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				//do nothing, we just need this to keep other onClickEvents from firing.
+				hideDrawer();
+			}
+			
+		});
+		
+		//create an array of ids for our various "addIcons" so we can keep this (mostly) dynamic.  
+		//Don't want to keep track of 9 different image views..
+		final int[] idArray = new int[9];
+		
+		for (int i = 1; i < 10; i++) {
+			String buttonLabel = "imageView" + i;
+			int arraySync = i-1;
+			
+			int resID = getResources().getIdentifier(buttonLabel, "id", DecorateActivity.this.getPackageName());
+			idArray[arraySync] = resID;
+		}
+		System.out.println("Finalized array is:  " + idArray.toString());
+		
+		for (int i = 0; i < idArray.length; i++) {
+			ImageView current = (ImageView) findViewById(idArray[i]);
+			final int temp = i;
+			current.setOnClickListener(new OnClickListener() {
+
+				@Override
+				public void onClick(View arg0) {
+					// TODO Auto-generated method stub					
+					enableDrawer(idArray[temp]);
+				}
+				
+			});
+		}
 	}
 	
 
@@ -46,6 +102,27 @@ public class DecorateActivity extends Activity{
 		super.finish();
 	}
 
+	//this method is responsible for displaying the drawer containing the icons the user can choose from.
+	//it receives the raw id of which imageView so we can dynamically do things in code, rather than keep 
+	//track of 9 different image views
+	public void enableDrawer(int Id) {
+		
+		currentSelected = Id;
+		System.out.println("ID pressed was:  " + Id);
+		finishBtn.setVisibility(View.GONE);
+		iconDrawer.setVisibility(View.VISIBLE);
+	}
 	
+	public void hideDrawer() {
+		iconDrawer.setVisibility(View.GONE);
+		finishBtn.setVisibility(View.VISIBLE);
+		removeListeners();
+	}
+
+	//this method removes all previously assigned clicklisteners so we aren't added images randomly
+	private void removeListeners() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
